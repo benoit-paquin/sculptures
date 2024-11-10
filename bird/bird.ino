68f3e3df1b3677ecc8a81065d05d017aff829b67
Benoit Paquin <benoit@paquin.dk>
2024-11-10 02:30:42 +0100

    initial commit

--- bird/bird.ino
+++ bird/bird.ino
@@ -1,46 +1,102 @@
 #include <avr/sleep.h> //Needed for sleep_mode
 #include <avr/wdt.h> //Needed to enable/disable watch dog timer
 #include <math.h>
-#define ledPin 1 
+#define ledPin 0 
 int i=0;
-int breathe_delay = 15;   // delay between loops
+int breathe_delay = 5;   // delay between loops
 unsigned long breathe_time = millis();
 byte redLed = 0;
 byte greenLed = 2;
+byte solarVcc = A3;
+byte yellowLed = 4;
+int oldVcc=9999;
+int vcc=0;
+
+void blink(byte led, byte cnt, int del) {
+  for (byte i = 0; i < cnt; i++) {
+    digitalWrite(led, HIGH);
+    goToSleep(3);
+    digitalWrite(led, LOW);
+    goToSleep(3); 
+  }
+}
 
 void setup() {
-  pinMode(0, OUTPUT);// LED connected to pin 5 which is recognised as pin 0 by arduino
-  pinMode(1, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(solarVcc, INPUT);
+  // test sequence
+  vcc = readVcc();
   //ADCSRA &= ~(1<<ADEN); //Disable ADC, saves ~230uA
   //Power down various bits of hardware to lower power usage  
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); //Power down everything, wake up from WDT
   sleep_enable();
+  blink(redLed,2,200);
+  blink(greenLed,2,200);
+  blink(yellowLed,2,200);
 }
 
 //This runs each time the watch dog wakes us up from sleep
 ISR(WDT_vect) {
   //watchdog_counter++;
 }
+
+void candleFlicker() {
+  // set the brightness of the LED based on the random number
+  analogWrite(yellowLed, random(0,220));
+  delay(random(10, 80));
+}
+
+void goToSleep(byte arg) {
+  setup_watchdog(arg);
+  sleep_mode();
+}
+
+void allOff() {
+  digitalWrite(redLed, LOW);
+  digitalWrite(yellowLed, LOW);
+  digitalWrite(greenLed, LOW);
+}
+
 void loop() {
-  int vcc;
-  vcc = readVcc();
-  if (false) { //vcc > 4500) {
-    nonBlockingBreath();
+  if (0 == i%(random(500,900))) {
+    allOff();
+    delay(2);
+    vcc=readVcc();
+    for(i = 0; i < random(1,5); i++) {
+      goToSleep(9);
+    }
+    i=1;
   }
-  else {
-    digitalWrite(greenLed, LOW);
-    digitalWrite(redLed, LOW);
-    setup_watchdog(7); //Setup watchdog to go off after 8sec
-    sleep_mode(); //Go to sleep! Wake up 4 sec later
-    int loopcnt = 2+roll(6);
-    for(int j=0;j<5;j++)
-    {
-      digitalWrite(redLed, (8<=roll(10)) ? HIGH : HIGH);
-      digitalWrite(greenLed,LOW);
-      delay(100+roll(100));
-      digitalWrite(redLed, LOW);
-      digitalWrite(greenLed,(8<=roll(10)) ? HIGH : HIGH);
-      delay(100+roll(100));
+  if (vcc < 2500) { // shudown to save capacitor
+    allOff();
+    oldVcc = vcc;
+    blink(redLed,2,100);
+    for(i = 0; i < 4; i++) {
+      goToSleep(9);
+    }    
+    vcc=readVcc();
+  } else if (vcc > oldVcc and vcc <3800) { //charge capacitor as there is sun
+    allOff();
+    oldVcc = vcc;
+    blink(greenLed,2,100);
+    goToSleep(9);
+    goToSleep(9);
+    vcc=readVcc();
+  } else {
+    if (analogRead(solarVcc) > 500) {
+      // it is sunny
+      allOff();
+      goToSleep(9);
+    } else {
+      // dark in the room
+      oldVcc = vcc;
+      nonBlockingBreath();//BreathIn();
+      candleFlicker();
+      if (0 == i%random(5,20)) {
+        digitalWrite(greenLed,9 < random(1,11)?HIGH:LOW);
+      }
     }
   }
 }
@@ -50,18 +106,16 @@ void loop() {
 //6=1sec, 7=2sec, 8=4sec, 9=8sec
 //From: http://interface.khm.de/index.php/lab/experiments/sleep_watchdog_battery/
 void setup_watchdog(int timerPrescaler) {
-
   if (timerPrescaler > 9 ) timerPrescaler = 9; //Limit incoming amount to legal settings
-
   byte bb = timerPrescaler & 7; 
   if (timerPrescaler > 7) bb |= (1<<5); //Set the special 5th bit if necessary
-
   //This order of commands is important and cannot be combined
   MCUSR &= ~(1<<WDRF); //Clear the watch dog reset
   WDTCR |= (1<<WDCE) | (1<<WDE); //Set WD_change enable, set WD enable
   WDTCR = bb; //Set new watchdog timeout value
   WDTCR |= _BV(WDIE); //Set the interrupt enable, this will keep unit from resetting after each int
 }
+
 long readVcc() {
   // Read 1.1V reference against AVcc
   // set the reference to Vcc and the measurement to the internal 1.1V reference
@@ -74,33 +128,24 @@ long readVcc() {
   #else
     ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
   #endif  
- 
   delay(2); // Wait for Vref to settle
   ADCSRA |= _BV(ADSC); // Start conversion
   while (bit_is_set(ADCSRA,ADSC)); // measuring
- 
   uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
   uint8_t high = ADCH; // unlocks both
- 
   long result = (high<<8) | low;
- 
   result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
   return result; // Vcc in millivolts
 }
  
+
 void nonBlockingBreath()
 {
   if( (breathe_time + breathe_delay) < millis() ){
     breathe_time = millis();
-    float val = (exp(sin(i/2000.0*PI*10)) - 0.36787944)*108.0; 
+    float val = (exp(sin(i/400.0*PI*10)) - 0.36787944)*108.0; 
     // this is the math function recreating the effect
-    analogWrite(ledPin, val);  // PWM
-    analogWrite(0, val);  // PWM
+    analogWrite(redLed, val);  // PWM
     i=i+1;
   }
-}
-int roll(int max)
-{
-   int random  = rand()%max+ 1;
-   return random;
 }
